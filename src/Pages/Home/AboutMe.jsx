0e8e2ff77678012823c00452697e202e03a64c91
alphajedidia <alphajedidia01@gmail.com>
2023-09-06 17:58:15 +0300

    portfolio Alpha Jedidia

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/AboutMe.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          As a student at the National School of Computer Science (ENI),
+           my passion for technology has been a lifelong pursuit. Now,
+            I'm following my dream. I'm not just focused on development but also on using 
+            IT in areas like marketing and growing businesses. 
+          </p>
+          <p className="hero--section-description">
+          As an engineer,
+             I prioritize problem-solving over technical skills,
+              valuing interpersonal skills above all.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-scroll";
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Alpha Jedidia </p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Aspiring Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          I am highly motivated to explore the vast world of IT and eager to collaborate on an IT project with you. Let's work together and take on new challenges!
+          </p><br />
+        </div>
+        <Link
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            to="Contact"
+            className="btn btn-primary"
+        >
+          Contact Me
+        </Link>    
+        <span>  </span>
+        <a href="your_cv.pdf" download="Alpha_Jedidia_CV.pdf">
+          <button className="btn btn-outline-primary">Download CV</button>
+        </a>
+
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/AlphaJedidiaIMG.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
